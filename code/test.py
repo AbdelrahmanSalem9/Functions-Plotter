5162ad690e8343598b989cb89af589e7791af9d2
AbdelrahmanSalem9 <abdelrahmansalem16@gmail.com>
2022-05-12 11:38:50 +0200

    testing functions updated

--- code/test.py
+++ code/test.py
@@ -1,5 +1,5 @@
 from app import proceed
-from constant import TEST_FUNCTIONS
+from testing_functions import TEST_FUNCTIONS
 
 for fx in TEST_FUNCTIONS:
     proceed(fx["function"], fx["x_min"], fx["x_max"])
---
+++ code/testing_functions.py
@@ -0,0 +1,75 @@
+TEST_FUNCTIONS = [
+    {
+        "function": "5*x^3 + 2*x",
+        "x_min": "-5",
+        "x_max": "8"
+
+    },
+    {
+        "function": "x^2 - 4",
+        "x_min": "-10",
+        "x_max": "10"
+
+    },
+    {
+        "function": "x^4 -9*x^2",
+        "x_min": "-4",
+        "x_max": "4"
+
+    },
+    {
+        "function": "x^3",
+        "x_min": "-1",
+        "x_max": "10"
+
+    },
+    {
+        "function": "2*x^2 -3*x + 5",
+        "x_min": "3",
+        "x_max": "8"
+
+    },
+    {
+        "function": "x + 2",
+        "x_min": "-2",
+        "x_max": "3"
+
+    },
+    {
+        "function": "e ** x",
+        "x_min": "-8",
+        "x_max": "10"
+
+    },
+    {
+        "function": "sin(2*x)",
+        "x_min": "-10",
+        "x_max": "10"
+
+    },
+    {
+        "function": "sin(x) + e**-x",
+        "x_min": "-20",
+        "x_max": "20"
+
+    },
+    {
+        "function": "cos(x) + sin(3*x +10) - 3*x^3",
+        "x_min": "-1",
+        "x_max": "20"
+
+    },
+    {
+        "function": "log10(2**x)",
+        "x_min": "5",
+        "x_max": "20"
+
+    },
+    {
+        "function": "log2(2**x) - 3*x + 8 - sin(5*x)",
+        "x_min": "3",
+        "x_max": "20"
+
+
+    },
+]
